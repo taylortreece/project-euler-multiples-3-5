8a14cdcf571e1529991f2d781059fc5bf9368944
taylortreece <tayloratreece@gmail.com>
2021-11-21 19:26:54 -0600

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,17 @@
 # Enter your procedural solution here!
+def collect_multiples(num)
+    array = []
+    for i in 1..num - 1
+        i % 3 == 0 || i % 5 == 0 ? array.push(i) : nil
+    end
+    return array
+end
+
+def sum_multiples(limit)
+    nums = collect_multiples(limit)
+    total = 0
+    for i in nums
+        total += i
+    end
+    return total
+end
